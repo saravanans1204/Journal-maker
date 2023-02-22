a26cf4c76f2db46c7daa7c4de6f677e43b556500
saravanans1204 <saravanans1204@gmail.com>
2023-02-22 16:59:27 +0530

    almost finished last 20 and 21 challenge

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const lo=require('lodash');
 const app=express();
 const PORT=process.env.PORT || 9000
 
@@ -11,27 +12,59 @@ app.use(express.static('public'));
 app.set('view engine','ejs');
 app.set('views',(__dirname+'/views'));
 
+const store=[{
+    title:"Marcus Aurelius",
+    content: `You have power over your mind  not outside events. Realize this, and you will find strength`
+}]
 
 
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 
 // routers
 app.get('/',(req,res)=>{
-    res.render('home',{page:"Home",content:homeStartingContent})
+    res.render('home',{page:"Home",content:store})
+    
 })
 
-app.get('/About',(req,res)=>{
+app.get('/about',(req,res)=>{
     res.render('about',{page:"About",content:aboutContent})
 })
 
 
-app.get('/Contacts',(req,res)=>{
+app.get('/contacts',(req,res)=>{
     res.render('contact',{page:"contact",content:contactContent})
 })
+
+app.get('/compose',(req,res)=>{
+    res.render('compose',{page:"contact",content:contactContent})
+})
+
+
+app.get('/:id',(req,res)=>{
+    const id=req.params.id
+
+    store.forEach((e)=>{
+       if(lo.lowerCase(e.title)===lo.lowerCase(id)){
+        res.render('expandpage',{title:e.title,content:e.content})
+       }
+    })
+    
+    
+})
+
+// post 
+app.post('/compose',(req,res)=>{
+    let post={
+        title:req.body.title,
+        content:req.body.entry
+    }
+    store.push(post)
+    res.redirect('/')
+})
+
+
 
 
 app.listen(PORT,()=>{
