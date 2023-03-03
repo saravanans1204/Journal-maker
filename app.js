4206733c161964b29d1d594259af6b9f5c2012e5
saravanans1204 <saravanans1204@gmail.com>
2023-03-03 14:03:09 +0530

    with delete button

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const store=[{
 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const click=(e)=>{
-    console.log(e)
+function click(e){
+    console.log('hello')
 }
  
 
@@ -68,6 +68,18 @@ app.post('/compose',(req,res)=>{
     res.redirect('/')
 })
 
+
+app.post('/:id',(req,res)=>{
+    const {id} = req.params
+
+    const deleteStore=store.findIndex(post=>post.title==id)
+
+    store.splice(deleteStore,1)
+
+    res.redirect('/')
+
+
+})
 
 
 
