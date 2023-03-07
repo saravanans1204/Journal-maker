d73daba0ae14f8e6db6edc08bcaff5f7ec1c5c71
saravanans1204 <saravanans1204@gmail.com>
2023-03-07 09:51:06 +0530

    for vercel

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // design 
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public/"));
 app.set('view engine','ejs');
 app.set('views',(__dirname+'/views'));
 let isLogin=false;
